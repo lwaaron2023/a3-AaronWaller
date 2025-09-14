2a523e5a7a9839647445bbfaf56d1ee00cc66107
lwaaron2023 <alwaller@wpi.edu>
2025-09-14 17:34:38 -0400

    Added more diverse form elements to main.html

--- public/js/main.js
+++ public/js/main.js
@@ -8,13 +8,53 @@ const submit = async function( event ) {
     const firstName = document.getElementById( "firstName" )
     const lastName = document.getElementById( "lastName" )
     const address = document.getElementById( "address" )
-    const shirts = document.getElementById( "shirts" )
-    const jackets = document.getElementById( "jackets" )
-    const hats = document.getElementById( "hats" )
+    const shirts = document.querySelector( 'input[name="numShirts"]:checked' )
+    const jackets = document.querySelector( 'input[name="numJackets"]:checked' )
+    const hats = document.querySelector( 'input[name="numHats"]:checked' )
 
     if(firstName && lastName && address && shirts && jackets && hats){
+        //checks to make sure the input own value is not checked
+        let s;
+        if(shirts.value < 0){
+            try{
+                s = document.getElementById( "s4t" ).value;
+            } catch(err){
+                console.log(err);
+                s = 0
+            }
+        }
+        else{
+            s = shirts.value;
+        }
+        //checks to make sure the input own value is not checked
+        let j;
+        if(jackets.value < 0){
+            try{
+                j = document.getElementById( "j4t" ).value;
+            } catch(err){
+                console.log(err);
+                j = 0
+            }
+        }
+        else{
+            j = jackets.value;
+        }
+        //checks to make sure the input own value is not checked
+        let h;
+        if(hats.value < 0){
+            try{
+                h = document.getElementById( "h4t" ).value;
+            } catch(err){
+                console.log(err);
+                h = 0
+            }
+        }
+        else{
+            h = hats.value;
+        }
+        console.log(`num shirts ${s}\n num jackets ${j}\n num hats ${h}`);
         //creates a json with the information contained within the form
-        const json = {firstName: firstName.value, lastName: lastName.value, address: address.value, shirts:shirts.value, jackets:jackets.value, hats:hats.value};
+        const json = {firstName: firstName.value, lastName: lastName.value, address: address.value, shirts:s, jackets:j, hats:h};
         const body = JSON.stringify(json);
         //sends a post request to the server
         await fetch( "/order/place", {
